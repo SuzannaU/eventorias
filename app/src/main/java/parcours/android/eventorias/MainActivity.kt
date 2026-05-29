64d4b5de4b1faaf9fbb3c6de7cd85ad6f4cabecf
SuzannaU <suzanna.ursulet@gmail.com>
2026-05-29 17:29:01 +0200

    FirebaseUI auth is setup and sign in with Google or email is working

--- app/src/main/java/parcours/android/eventorias/MainActivity.kt
+++ app/src/main/java/parcours/android/eventorias/MainActivity.kt
@@ -1,31 +1,78 @@
 package parcours.android.eventorias
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import parcours.android.eventorias.ui.theme.EventoriasTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract(),
+        ::onSignInResult,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             EventoriasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier.padding(innerPadding),
+                    ) {
+                        Greeting(
+                            name = "Android",
+                        )
+                        Button(
+                            onClick = { startSignInActivity() }
+                        ) {
+                            Text(text = "Se connecter")
+                        }
+                    }
                 }
             }
+        }
+    }
+
+    private fun startSignInActivity() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setTheme(R.style.Theme_Eventorias_Login)
+            .setAvailableProviders(providers)
+            .setAlwaysShowSignInMethodScreen(true)
+            .setLogo(R.drawable.logo_eventorias)
+            .build()
+
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            Log.d("TAG", "user signed in")
+            //viewModel.createUser()
+        } else if (response?.error != null) {
+            Log.d("TAG", "Error while signing in: ${response.error?.message}")
         }
     }
 }
